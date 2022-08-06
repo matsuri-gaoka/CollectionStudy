bdc592aacfd88acfcc57543e71c8134fc8c0cac2
matsuri-gaoka <repetitive789++@gmail.com>
2022-08-06 23:54:26 +0900

    insertRangeを拡張関数にした

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,20 @@
 fun main(args: Array<String>) {
     val list1 = listOf(1, 2, 3, 4, 5)
     val list2 = listOf(99, 98, 97)
-    printList(insertRange(list1, list2, 2))
+    printList(list1.insertRange(list2, 2))
 }
 
 fun <T> insertRange(original: List<T>, insert: List<T>, index: Int): List<T> {
     if(index < 0 || index > original.size){
-        throw IllegalArgumentException()
+        //throw IllegalArgumentException()
     }
     return original.subList(0, index) +
             insert + original.subList(index, original.size)
+}
+
+@JvmName("insertRangeT")
+fun <T> List<T>.insertRange(insert: List<T>, index: Int): List<T> {
+    return insertRange(this, insert, index)
 }
 
 fun <T> printList(list: List<T>) {
